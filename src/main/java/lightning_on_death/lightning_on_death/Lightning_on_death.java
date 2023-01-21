72e09a91b7bc8bebf100ec1d6096bdc4f24f2fa3
Recho1235 <86173011+Recho1235@users.noreply.github.com>
2023-01-21 02:16:19 +0000

    Add files via upload

---
+++ src/main/java/lightning_on_death/lightning_on_death/Lightning_on_death.java
@@ -0,0 +1,21 @@
+package lightning_on_death.lightning_on_death;
+
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+
+public class Lightning_on_death extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent event) {
+        Player player = event.getEntity();
+        player.getWorld().strikeLightningEffect(player.getLocation());
+    }
+}
